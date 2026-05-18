09ae89b659cef06209a9e3948404c67d59900986
kimmo <kimmo.ahola@systementor.se>
2026-05-18 13:04:46 +0200

    trigger or not?

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.routers import tasks
 # vi ska ersätta med alembic nästa lektion
 
 app = FastAPI(
-    title="My \"real production\" api",
+    title="My \"real production style\" api",
     description="""
 A simple tasks API built as a demo.
 
