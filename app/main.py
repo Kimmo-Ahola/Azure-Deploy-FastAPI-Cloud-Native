3fdc9412c6238060314b3d08b74efc0d4e0a9632
kimmo <kimmo.ahola@systementor.se>
2026-05-18 13:50:57 +0200

    commit msg

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.routers import tasks
 # vi ska ersätta med alembic nästa lektion
 
 app = FastAPI(
-    title='My "tesssssssssssssst" api',
+    title='My "Real" api',
     description="""
 A simple tasks API built as a demo.
 
