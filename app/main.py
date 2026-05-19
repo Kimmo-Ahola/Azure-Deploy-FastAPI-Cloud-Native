e57278c54462a44ec94ac7b10704c6444fba00d3
kimmo <kimmo.ahola@systementor.se>
2026-05-19 16:39:08 +0200

    testing an UI change

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.routers import tasks
 # vi ska ersätta med alembic nästa lektion
 
 app = FastAPI(
-    title='My "Real" api',
+    title='My "Fake" api',
     description="""
 A simple tasks API built as a demo.
 
